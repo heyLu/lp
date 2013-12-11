a437349cf0e72f0b0a827421257dcccca66a87b9
Lucas Stadler <mi.au@papill0n.org>
2013-12-11 15:36:16 +0100

    Simple demo of using friendly-check from clojurescript.

---
+++ clj/clarity/cljs/clarity/client.cljs
@@ -0,0 +1,13 @@
+(ns clarity.client
+  (:use [cljs.reader :only (read-string)])
+
+  (:require [clarity.types :as t]))
+
+(def query-field (js/document.querySelector "#query"))
+(def query-result (js/document.querySelector "#query-type-check"))
+
+(.addEventListener query-field "input"
+  (fn [e]
+    (let [content (.-value query-field)
+          check   (t/friendly-check (read-string content) t/datomic-attr-type)]
+      (set! (.-textContent query-result) check))))
--- clj/clarity/project.clj
+++ clj/clarity/project.clj
@@ -1,6 +1,7 @@
 (defproject clarity "0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.typed "0.2.19"]
+                 [org.clojure/clojurescript "0.0-2080"]
 
                  [com.datomic/datomic-free "0.9.4331"]
 
@@ -9,5 +10,9 @@
 
                  [hiccup "1.0.4"]]
   :source-paths ["."]
-  :plugins [[lein-ring "0.8.8"]]
-  :ring {:handler clarity.server/app})
+  :plugins [[lein-ring "0.8.8"]
+            [lein-cljsbuild "1.0.1-SNAPSHOT"]]
+  :ring {:handler clarity.server/app}
+  :cljsbuild {:crossovers [clarity.types]
+              :crossover-path "target/crossover"
+              :builds [{:source-paths ["cljs"]}]})
