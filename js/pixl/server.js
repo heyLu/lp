d03cc456439b74d62342dd8fc647e23470059469
Lucas Stadler <mi.au@papill0n.org>
2013-12-17 13:42:54 +0100

    send and receive arrays of pixls.
    preparation for possible batching later.

--- js/pixl/server.js
+++ js/pixl/server.js
@@ -8,10 +8,11 @@ server.on('connection', function(socket) {
 	socket.send(JSON.stringify(world));
 
 	socket.on('message', function(msg) {
-		var pixl = JSON.parse(msg);
-		world[pixl.x + "," + pixl.y] = {color: pixl.color};
-
-		console.log(pixl);
+		var pixls = JSON.parse(msg);
+		pixls.forEach(function(pixl) {
+			world[pixl.x + "," + pixl.y] = {color: pixl.color};
+			console.log(pixl);
+		});
 
 		server.clients.forEach(function(client) {
 			if (socket != client) {
