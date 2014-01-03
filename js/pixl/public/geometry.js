458d8aaa69d81fa32af03ab426d339c82112679b
Lucas Stadler <mi.au@papill0n.org>
2014-01-03 18:35:39 +0100

    simple lighting from a static directional source + ambient.
    https://developer.mozilla.org/en-US/docs/Web/WebGL/Lighting_in_WebGL

--- js/pixl/public/geometry.js
+++ js/pixl/public/geometry.js
@@ -51,3 +51,19 @@ geometry.cube = function() {
 	];
 };
 
+geometry.cube.normals = function() {
+	var front = [0.0, 0.0, 1.0],
+	    back = [0.0, 0.0, -1.0],
+	    left = [-1.0, 0.0, 0.0],
+	    right = [1.0, 0.0, 0.0],
+	    top = [0.0, 1.0, 0.0],
+	    bottom = [0.0, -1.0, 0.0];
+
+	return [].concat(
+		front, front, front, front, front, front,
+		back, back, back, back, back, back,
+		left, left, left, left, left, left,
+		right, right, right, right, right, right,
+		top, top, top, top, top, top,
+		bottom, bottom, bottom, bottom, bottom, bottom);
+}
