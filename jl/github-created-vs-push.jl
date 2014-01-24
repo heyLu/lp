0fb5a891dbf2f1a1fa64ac62aa67d444db37a38b
Lucas Stadler <mi.au@papill0n.org>
2014-01-24 12:30:11 +0100

    explain a bit what the github fetching thing does.

--- jl/github-created-vs-push.jl
+++ jl/github-created-vs-push.jl
@@ -3,6 +3,24 @@ using JSON
 using DataFrames
 using Gadfly
 
+# this started out as an experiment, just trying to see what i can do
+# with julia and some json.
+#
+# this will generate a graph of the times repositories of a user were
+# created vs the last push times to that repository.
+#
+# it started out as a silly experiment that simply plotted two
+# numeric/ordered values from the data, but it's quite interesting:
+# quite a few users have this pattern where they constantly create (or
+# fork) repositories and then move on after a while. for most people
+# and projects we see a roughly linear increase, suggesting that people
+# continue creating repositories on github and work a bit on them and
+# then move on.
+# still, there are projects that were started early and are still active,
+# for example mbostock/d3 or my own heyLu/confidence (my dotfiles)
+#
+# next up: having fun with commits? (or activity?)
+
 github_get(path) = takebuf_string(get(string("https://api.github.com", path), headers=[("User-Agent", "Julia-Experiment")]).body)
 
 function toDataFrame(dicts, keys = keys(dicts[1]))
