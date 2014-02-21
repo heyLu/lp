89f17fe9bdab91a17166e42357664f3b4bfa127c
Lucas Stadler <mi.au@papill0n.org>
2014-02-21 13:50:46 +0100

    create clojure project with datomic samples as checkout deps.

---
+++ clj/datomic/project.clj
@@ -0,0 +1,7 @@
+(defproject datomic-tutorial "0.0-alpha"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.4556"]
+
+                 ; for convenience
+                 [com.datomic/mbrainz-sample "0.1.0"]
+                 [day-of-datomic "1.0.0-SNAPSHOT"]])
