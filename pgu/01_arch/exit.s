eeb00ed130cb8f92f97216941bdeb3765bda345c
Lucas Stadler <lu@papill0n.org>
2014-03-28 12:21:31 +0100

    document what exit.s does.

--- pgu/01_arch/exit.s
+++ pgu/01_arch/exit.s
@@ -1,11 +1,19 @@
+# exit: a program that calls `exit()`
+
+# an assembler program has sections, this section normally has data in
+# it that the program uses. (i think this would be variables in any
+# other language, but probably there's more.)
 .section .data
 
+# the program text lives here.
 .section .text
-.globl _start
+.globl _start # a special symbol that the linker uses to start the program
 
+# call `exit(0)`
 _start:
-	movl $1, %eax
+	# $<something> is the syntax for the immediate addressing mode
+	movl $1, %eax # exit has the syscall number 1, defined in /usr/include/asm/unistd_32.h
 
-	movl $0, %ebx
+	movl $0, %ebx # pass 0 as an argument
 
-	int $0x80
+	int $0x80     # 0x80 is the syscall interupt for the kernel
