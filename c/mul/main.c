f74e5ce97694c81f40182e8e4441f397752eddbc
Lucas Stadler <lu@papill0n.org>
2014-04-04 19:40:31 +0200

    eval simple numeric expressions.

--- c/mul/main.c
+++ c/mul/main.c
@@ -4,6 +4,31 @@
 
 #include "mpc.h"
 
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	char* op = t->children[1]->contents;
+	long x = eval(t->children[2]);
+
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 int main(int argc, char** argv) {
 	mpc_parser_t* Number = mpc_new("number");
 	mpc_parser_t* Operator = mpc_new("operator");
@@ -27,7 +52,10 @@ lang     : /^/ <expr>+ /$/ ; \
 
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lang, &r)) {
-			mpc_ast_print(r.output);
+			mpc_ast_t* t = r.output;
+			mpc_ast_print(t->children[1]);
+			long result = eval(t->children[1]);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
