41355ef886ad24ba23ce9073d016d4bdf75a2153
Lucas Stadler <lu@papill0n.org>
2014-04-04 18:17:06 +0200

    rewrite using the editline library.
    tiny line editing, dunno why we're not using readline, probably because
    of some licensing stuff.

--- c/mul/main.c
+++ c/mul/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
+#include <editline/readline.h>
+
 int main(int argc, char** argv) {
 	puts("mul v0.0.1\n");
 
 	while (1) {
-		fputs("> ", stdout);
-
-		fgets(input, 2048, stdin);
+		char* input = readline("> ");
+		add_history(input);
 
 		printf("%s, yes.", input);
+
+		free(input);
 	}
 
 	return 0;
