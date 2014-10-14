af9013be8c13c9e3517f7ddd76494c0e08749ea4
Lucas Stadler <lu@papill0n.org>
2014-10-14 18:08:37 +0200

    don't use references for now.
    they will return when i understand them a bit.

--- rust/list.rs
+++ rust/list.rs
@@ -4,7 +4,6 @@ pub enum List<A> {
     Cons(A, Box<List<A>>)
 }
 
-// xs: doesn't work as a reference, probably as Box?
 pub fn cons<A>(x: A, xs: List<A>) -> List<A> {
     match xs {
         Nil => Cons(x, box Nil),
@@ -12,22 +11,16 @@ pub fn cons<A>(x: A, xs: List<A>) -> List<A> {
     }
 }
 
-pub fn first<A>(xs: &List<A>) -> Option<&A> {
+pub fn first<A>(xs: List<A>) -> Option<A> {
     match xs {
-        &Nil => None,
-        &Cons(ref x, _) => Some(x)
+        Nil => None,
+        Cons(x, _) => Some(x)
     }
 }
 
-// nth
-// map - does this make sense? (e.g. we probably don't want to copy everything.) should it be lazy?
-//   fn map(..., &List<&A>) -> List<B> // or -> List<&B>?
-// filter - we want references to the old values and keep the existing cons cells
-// reduce
-
 pub fn main() {
-	let nil: &List<int> = &Nil;
-	let ns:  &List<int> = &cons(1i, cons(2, cons(3, Nil)));
+	let nil: List<int> = Nil;
+	let ns:  List<int> = cons(1i, cons(2, cons(3, Nil)));
 
 	println!("nil = {}, ns = {}", nil, ns);
 	println!("first(nil) = {}, first(ns) = {}", first(nil), first(ns));
