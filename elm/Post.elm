c3fbb6e515bdd190eaf60708a10f8da8303568c2
Lucas Stadler <lu@papill0n.org>
2015-03-01 15:20:56 +0100

    transform newlines into paragraphs.

--- elm/Post.elm
+++ elm/Post.elm
@@ -2,9 +2,11 @@ module Post where
 
 import Date
 import Date (Date)
+import List
 import Html (..)
 import Util.Compare (compareBy, flipCompare)
 import Util.Html (viewDate)
+import String
 
 type alias Post = { title:String, content:String, created:Date }
 
@@ -12,8 +14,10 @@ compareByDate = compareBy (.created >> Date.toTime)
 compareByDateReverse = flipCompare <| compareBy (.created >> Date.toTime)
 
 view : Date -> Post -> Html
-view ref post = div [] [
-                 h3 [] [text post.title],
-                 p [] [text post.content],
-                 span [] [text "Written ", viewDate ref post.created]
-                ]
+view ref post =
+    let paragraphs = List.filter (not << String.isEmpty) <| String.split "\n" post.content
+    in div [] [
+            h3 [] [text post.title],
+            section [] (List.map (\par -> p [] [text par]) paragraphs),
+            span [] [text "Written ", viewDate ref post.created]
+           ]
--- elm/Stream.elm
+++ elm/Stream.elm
@@ -17,13 +17,14 @@ date s = case (Date.fromString s) of
     Err e -> Date.fromTime 0
 
 posts : List Post
-posts = [{title = "Something else", content = "Well, I can say more than \"Hello\", I guess!", created = date "2015-03-01T14:03"},
+posts = [{title = "Fancy post", content = "Post may have multiple lines now.\nWhat freedom!\n\n\nThat's weird, though...", created = date "2015-03-01T15:18"},
+         {title = "Something else", content = "Well, I can say more than \"Hello\", I guess!", created = date "2015-03-01T14:03"},
          {title = "Hello, Other Things?", content = "There are other things?", created = date "2015-03-01T12:53:21"},
          {title = "Hello, World!", content = "This is my very first post!", created = date "2015-03-01T12:27:00"},
          {title = "Blog setup", content = "I guess I should post something now?", created = date "2014-12-24T10:57:03"},
          {title = "Ancient history", content = "Teenage angst!!!!", created = date "2009-06-07T02:54:29"}
         ]
 
-referenceDate = date "2015-03-01T14:09"
+referenceDate = date "2015-03-01T15:20"
 
 main = div [] (List.map (Post.view referenceDate) (List.sortWith Post.compareByDateReverse posts))
