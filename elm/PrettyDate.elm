a3ab85f33a1a4f2a10ad7a96cb3de11964642794
Lucas Stadler <lu@papill0n.org>
2015-03-01 19:06:00 +0100

    make the prettyDate implementation generic
    there is a `RelativeUnit` type now, which is pattern-matched on and then
    the result of that is used in the generic formatting.

--- elm/PrettyDate.elm
+++ elm/PrettyDate.elm
@@ -10,39 +10,39 @@ prettyDate reference date =
         diffSeconds = referenceSeconds - dateSeconds
     in format << toRelativeTime <| diffSeconds
 
-type RelativeTime = JustNow
-                  | Minute Int
-                  | Hour Int
-                  | Day Int
-                  | Week Int
-                  | Month Int
-                  | Year Int
-                  | Future RelativeTime
+type RelativeUnit = JustNow
+                  | Minute
+                  | Hour
+                  | Day
+                  | Week
+                  | Month
+                  | Year
+
+type RelativeTime = Past RelativeUnit Int
+                  | Future RelativeUnit Int
+
+stringForm relativeUnit =
+    case relativeUnit of
+      Minute  -> ("a minute", "minutes")
+      Hour    -> ("an hour", "hours")
+      Day     -> ("a day", "days")
+      Week    -> ("a week", "weeks")
+      Month   -> ("a month", "months")
+      Year    -> ("a year", "years")
 
 format : RelativeTime -> String
 format relativeTime =
     case relativeTime of
-      JustNow -> "moments ago"
-      Minute 1 -> "a minute ago"
-      Minute n -> toString n ++ " minutes ago"
-      Hour 1 -> "hour ago"
-      Hour n -> toString n ++ " hours ago"
-      Day 1 -> "a day ago"
-      Day n -> toString n ++ " years ago"
-      Week 1 -> "a week ago"
-      Week n -> toString n ++ " weeks ago"
-      Month 1 -> "a month ago"
-      Month n -> toString n ++ " months ago"
-      Year 1 -> "a year ago"
-      Year n -> toString n ++ "years ago"
-      Future (JustNow) -> "in a moment"
-      Future (Minute n) -> "in " ++ toString n ++ " hours"
-      Future (Hour n) -> "in " ++ toString n ++ " hours"
-      Future (Day n) -> "in " ++ toString n ++ " days"
-      Future (Week n) -> "in " ++ toString n ++ " weeks"
-      Future (Month n) -> "in " ++ toString n ++ " months"
-      Future (Year n) -> "in " ++ toString n ++ " years"
-      _ -> "???"
+      Past   JustNow _ -> "moments ago"
+      Future JustNow _ -> "in a moment"
+      Past   unit n    -> format' ""    " ago" unit n
+      Future unit n    -> format' "in " ""     unit n
+
+format' prefix suffix unit n =
+    let (singular, plural) = stringForm unit
+    in if n == 1
+       then prefix ++ singular
+       else prefix ++ toString n ++ " " ++ plural ++ suffix
 
 toRelativeTime : Float -> RelativeTime
 toRelativeTime origDiff =
@@ -55,14 +55,14 @@ toRelativeTime origDiff =
         roundToString = toString << ceiling
         inThePast = origDiff > 0
         diff = abs origDiff
-        relativeTime =
-            if | diff < minute -> JustNow
-               | diff < hour   -> Minute <| round (diff / minute)
-               | diff < day    -> Hour <| round (diff / hour)
-               | diff < week   -> Day <| round (diff / day)
-               | diff < month  -> Week <| round (diff / week)
-               | diff < year   -> Month <| round (diff / month)
-               | otherwise     -> Year <| round (diff / year)
+        (relativeUnit, n) =
+            if | diff < minute -> (JustNow, 0)
+               | diff < hour   -> (Minute, round (diff / minute))
+               | diff < day    -> (Hour, round (diff / hour))
+               | diff < week   -> (Day, round (diff / day))
+               | diff < month  -> (Week, round (diff / week))
+               | diff < year   -> (Month, round (diff / month))
+               | otherwise     -> (Year, round (diff / year))
     in if inThePast
-       then relativeTime
-       else Future relativeTime
+       then Past relativeUnit n
+       else Future relativeUnit n
