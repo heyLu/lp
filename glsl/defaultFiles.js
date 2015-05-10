4cb58c6ae3f41389bb5fba3fee145465d15c3b48
Lucas Stadler <lu@papill0n.org>
2015-05-10 14:33:39 +0200

    split up the default files into (quite a few) small parts
    e.g. the primitives from different sources (iq, cupe) have their own
    includes now, as does the sphere tracer and the camera from iq's
    "raymarching primitives" on shadertoy.

--- glsl/defaultFiles.js
+++ glsl/defaultFiles.js
@@ -5,7 +5,44 @@ files.builtin = {
 void main() {
   gl_FragColor = vec4(gl_FragCoord.xy / iResolution, blue, 1.0);
 }`,
-  "default.frag": `const int MaximumRaySteps = 150;
+  "default.frag": `//#include "includes/sphere-tracer.frag"
+//#include "includes/iq-primitives.frag"
+//#include "includes/cupe-primitives.frag"
+
+uniform vec3 offset; //#slider[(0.0,10.0,20.0),(0.0,10.0,20.0),(0.0,2.5,20.0)]
+
+float DistanceEstimator(vec3 pos) {
+  pMod1(pos.x, offset.x);
+  pMod1(pos.y, offset.y);
+  pMod1(pos.z, offset.z + sin(iGlobalTime));
+  //return sphere(pos);
+  //return min(sphere(vec3(pos.x, pos.y - 0.5, pos.z), 0.75),
+  //           udBox(pos, vec3(1.0, 0.3, 1.0)));
+  return min(max(-sphere(pos), udBox(pos, vec3(0.75))),
+             sphere(pos, 0.05 + 0.25 * (1.0 + sin(iGlobalTime * 0.5)*0.5)));
+}
+
+//#include "includes/default-main.frag"
+`,
+  "includes/iq-primitives.frag": `float sphere(vec3 pos) {
+  return length(pos) - 1.0;
+}
+
+float sphere(vec3 pos, float size) {
+  return length(pos) - size;
+}
+
+float udBox( vec3 p, vec3 b ) {
+  return length(max(abs(p)-b,0.0));
+}
+`,
+  "includes/cupe-primitives.frag": `float pMod1(inout float p, float size) {
+  float halfsize = size * 0.5;
+  float c = floor((p + halfsize)/size);
+  p = mod(p+halfsize, size)-halfsize;
+  return c;
+}`,
+  "includes/sphere-tracer.frag": `const int MaximumRaySteps = 150;
 const float MinimumDistance = 0.0001;
 
 float DistanceEstimator(vec3 pos);
@@ -21,41 +58,8 @@ float trace(vec3 from, vec3 direction) {
 		if (distance < MinimumDistance) break;
 	}
 	return 1.0-float(stepsDone)/float(MaximumRaySteps);
-}
-
-float sphere(vec3 pos) {
-  return length(pos) - 1.0;
-}
-
-float sphere(vec3 pos, float size) {
-  return length(pos) - size;
-}
-
-float udBox( vec3 p, vec3 b ) {
-  return length(max(abs(p)-b,0.0));
-}
-
-float pMod1(inout float p, float size) {
-  float halfsize = size * 0.5;
-  float c = floor((p + halfsize)/size);
-  p = mod(p+halfsize, size)-halfsize;
-  return c;
-}
-
-uniform vec3 offset; //#slider[(0.0,10.0,20.0),(0.0,10.0,20.0),(0.0,2.5,20.0)]
-
-float DistanceEstimator(vec3 pos) {
-  pMod1(pos.x, offset.x);
-  pMod1(pos.y, offset.y);
-  pMod1(pos.z, offset.z + sin(iGlobalTime));
-  //return sphere(pos);
-  //return min(sphere(vec3(pos.x, pos.y - 0.5, pos.z), 0.75),
-  //           udBox(pos, vec3(1.0, 0.3, 1.0)));
-  return min(max(-sphere(pos), udBox(pos, vec3(0.75))),
-             sphere(pos, 0.05 + 0.25 * (1.0 + sin(iGlobalTime * 0.5)*0.5)));
-}
-
-mat3 setCamera( in vec3 ro, in vec3 ta, float cr ) {
+}`,
+  "includes/default-main.frag": `mat3 setCamera( in vec3 ro, in vec3 ta, float cr ) {
 	vec3 cw = normalize(ta-ro);
 	vec3 cp = vec3(sin(cr), cos(cr),0.0);
 	vec3 cu = normalize( cross(cw,cp) );
