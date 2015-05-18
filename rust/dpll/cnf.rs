32e45b892bb9e88d8a77095f3977be768eba8051
Lucas Stadler <lu@papill0n.org>
2015-05-18 17:00:32 +0200

    actually return something from parse_dimac

--- rust/dpll/cnf.rs
+++ rust/dpll/cnf.rs
@@ -10,27 +10,32 @@
 use std::io;
 use std::io::Read;
 
-fn parse_dimac(dimac: &str) {
+struct CNF {
+    num_vars: u32,
+    num_clauses: u32,
+    clauses: Vec<Vec<i32>>
+}
+
+fn parse_dimac(dimac: &str) -> Result<CNF, String> {
     let mut lines = dimac.lines();
     let mut num_vars;
     let mut num_clauses;
     
     match lines.next() {
-        None => { println!("Error: expected cnf description"); return }
+        None => { return Err("Error: expected cnf description".to_string()) }
         Some(line) => {
             let desc: Vec<&str> = line.split(" ").collect();
             if desc.len() != 4 || desc[0] != "p" || desc[1] != "cnf" {
-                println!("Error: cnf description must be of the form 'p cnf <num vars> <num clauses>'");
-                return;
+                return Err("Error: cnf description must be of the form 'p cnf <num vars> <num clauses>'".to_string())
             }
             match desc[2].parse::<u32>() {
                 Ok(n) => { num_vars = n }
-                Err(e) => { println!("Error: <num vars> must be a positive integer: {}", e); return; }
+                Err(e) => { return Err(format!("Error: <num vars> must be a positive integer: {}", e)) }
             }
             
             match desc[3].parse::<u32>() {
                 Ok(n) => { num_clauses = n }
-                Err(e) => { println!("Error: <num clauses> must be a positive integer: {}", e); return; }
+                Err(e) => { return Err(format!("Error: <num clauses> must be a positive integer: {}", e)) }
             }
             println!("cnf has {} variables and {} clauses", num_vars, num_clauses)
         }
@@ -38,32 +43,35 @@ fn parse_dimac(dimac: &str) {
 
     let clause_lines: Vec<&str> = lines.collect();
     if clause_lines.len() as u32 != num_clauses {
-        println!("Error: Wrong number of clauses: Expected {}, but got {}", num_clauses, clause_lines.len());
-        return
+        return Err(format!("Error: Wrong number of clauses: Expected {}, but got {}", num_clauses, clause_lines.len()))
     }
 
     let mut clauses: Vec<Vec<i32>> = Vec::with_capacity(num_clauses as usize);
     for clause_line in clause_lines {
         let mut vars: Vec<i32> = clause_line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect();
         if vars.is_empty() {
-            println!("Error: empty clause");
-            return
+            return Err("Error: empty clause".to_string())
         }
         if vars[vars.len()-1] != 0 {
-            println!("Clause must be terminated with 0");
-            return
+            return Err("Clause must be terminated with 0".to_string())
         }
         let l = vars.len();
         vars.truncate(l - 1);
         println!("{:?}", vars);
         clauses.push(vars)
     }
+
+    let cnf = CNF { num_vars: num_vars, num_clauses: num_clauses, clauses: clauses };
+    return Ok(cnf)
 }
 
 fn main() {
     let input: &mut String = &mut String::new();
     match io::stdin().read_to_string(input) {
-        Ok(_) => { parse_dimac(input) }
+        Ok(_) => match parse_dimac(input) {
+            Ok(cnf) => { println!("ok!") }
+            Err(e) => { println!("{}", e) }
+        },
         Err(e) => { println!("Error: {}", e) }
     }
 }
