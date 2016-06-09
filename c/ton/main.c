97e800fa5fe19d3fe268540a5d27a98987d6a8d6
Lucas Stadler <lu@papill0n.org>
2016-06-09 18:03:31 +0200

    Get much closer to get something real up and running
    This is about a day (and a half?) of work, mostly spent trying to
    understand what planck is doing, and then porting code over,
    occasionally chasing segfaults.
    
    We can evaluate ClojureScript expressions now, but not require the
    planck namespaces yet.  The errors point to missing files, but I'm
    guessing that some of the `function_*` ports are incomplete.

--- c/ton/main.c
+++ c/ton/main.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,11 +15,26 @@ char console_log_buf[CONSOLE_LOG_BUF_SIZE];
 JSStringRef to_string(JSContextRef ctx, JSValueRef val);
 JSValueRef evaluate_script(JSContextRef ctx, char *script, char *source);
 
+void evaluate_source(JSContextRef ctx, char *type, char *source_value, bool expression, char *set_ns);
 char *munge(char *s);
 
 void bootstrap(JSContextRef ctx, char *deps_file_path, char *goog_base_path);
+JSObjectRef get_function(JSContextRef ctx, char *namespace, char *name);
 
 char* get_contents(char *path);
+
+void debug_print_value(char *prefix, JSContextRef ctx, JSValueRef exception) {
+#ifdef DEBUG
+	if (exception != NULL) {
+		JSStringRef ex_str = to_string(ctx, exception);
+		char ex_buf[1000];
+		ex_buf[0] = '\0';
+		JSStringGetUTF8CString(ex_str, ex_buf, 1000-1);
+		printf("%s: %s\n", prefix, ex_buf);
+		JSStringRelease(ex_str);
+	}
+#endif
+}
 
 JSValueRef function_console_log(JSContextRef ctx, JSObjectRef function, JSObjectRef this_object,
 		size_t argc, const JSValueRef args[], JSValueRef* exception) {
@@ -49,6 +66,131 @@ JSValueRef function_console_error(JSContextRef ctx, JSObjectRef function, JSObje
 	fprintf(stderr, "\n");
 
 	return JSValueMakeUndefined(ctx);
+}
+
+JSValueRef function_read_file(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	// TODO: implement fully
+	fprintf(stderr, "WARN: %s: stub\n", __func__);
+
+	if (argc == 1 && JSValueGetType(ctx, args[0]) == kJSTypeString) {
+		char path[100];
+		JSStringRef path_str = JSValueToStringCopy(ctx, args[0], NULL);
+		JSStringGetUTF8CString(path_str, path, 100);
+		JSStringRelease(path_str);
+
+		debug_print_value
+	("read_file", ctx, args[0]);
+
+		char full_path[150];
+		// TODO: should not load from here?
+		snprintf(full_path, 150, "%s/%s", "out", path);
+
+		char *contents = get_contents(full_path);
+		JSStringRef contents_str = JSStringCreateWithUTF8CString(contents);
+		free(contents);
+
+		JSValueRef res[2];
+		res[0] = JSValueMakeString(ctx, contents_str);
+		res[1] = JSValueMakeNumber(ctx, 0);
+		return JSObjectMakeArray(ctx, 2, res, NULL);
+	}
+
+	return JSValueMakeNull(ctx);
+}
+
+JSValueRef function_load(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	// TODO: implement fully
+	fprintf(stderr, "WARN: %s: stub\n", __func__);
+
+	if (argc == 1 && JSValueGetType(ctx, args[0]) == kJSTypeString) {
+		char path[100];
+		JSStringRef path_str = JSValueToStringCopy(ctx, args[0], NULL);
+		JSStringGetUTF8CString(path_str, path, 100);
+		JSStringRelease(path_str);
+
+		debug_print_value
+	("load", ctx, args[0]);
+
+		char full_path[150];
+		// TODO: should not load from here?
+		snprintf(full_path, 150, "%s/%s", "out", path);
+
+		char *contents = get_contents(full_path);
+		JSStringRef contents_str = JSStringCreateWithUTF8CString(contents);
+		free(contents);
+
+		JSValueRef res[2];
+		res[0] = JSValueMakeString(ctx, contents_str);
+		res[1] = JSValueMakeNumber(ctx, 0);
+		return JSObjectMakeArray(ctx, 2, res, NULL);
+	}
+
+	return JSValueMakeNull(ctx);
+}
+
+JSValueRef function_load_deps_cljs_files(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	// TODO: not implemented
+	fprintf(stderr, "WARN: %s: stub\n", __func__);
+	return JSObjectMakeArray(ctx, 0, NULL, NULL);
+}
+
+JSValueRef function_eval(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	JSValueRef val = NULL;
+
+	if (argc == 2
+		&& JSValueGetType(ctx, args[0]) == kJSTypeString
+		&& JSValueGetType(ctx, args[1]) == kJSTypeString) {
+		debug_print_value("eval", ctx, args[1]);
+
+		JSStringRef sourceRef = JSValueToStringCopy(ctx, args[0], NULL);
+		JSStringRef pathRef = JSValueToStringCopy(ctx, args[1], NULL);
+
+		JSEvaluateScript(ctx, sourceRef, NULL, pathRef, 0, &val);
+
+		JSStringRelease(pathRef);
+		JSStringRelease(sourceRef);
+	}
+
+	return val != NULL ? val : JSValueMakeNull(ctx);
+}
+
+JSValueRef function_get_term_size(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	// TODO: not implemented
+	fprintf(stderr, "WARN: %s: stub\n", __func__);
+	return JSValueMakeNull(ctx);
+}
+
+JSValueRef function_print_fn(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	if (argc == 1 && JSValueIsString(ctx, args[0])) {
+		JSStringRef val_str = JSValueToStringCopy(ctx, args[0], NULL);
+		char buf[1000];
+		JSStringGetUTF8CString(val_str, buf, 1000);
+
+		fprintf(stdout, "%s", buf);
+		fflush(stdout);
+	}
+
+	return JSValueMakeNull(ctx);
+}
+
+JSValueRef function_print_err_fn(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+		size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	if (argc == 1 && JSValueIsString(ctx, args[0])) {
+		JSStringRef val_str = JSValueToStringCopy(ctx, args[0], NULL);
+		char buf[1000];
+		JSStringGetUTF8CString(val_str, buf, 1000);
+
+		fprintf(stderr, "%s", buf);
+		fflush(stderr);
+	}
+
+	return JSValueMakeNull(ctx);
 }
 
 JSValueRef function_import_script(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
@@ -95,7 +237,10 @@ int main(int argc, char **argv) {
 
 	JSObjectRef global_obj = JSContextGetGlobalObject(ctx);
 
+	evaluate_script(ctx, "var global = this;", "<init>");
+
 	register_global_function(ctx, "IMPORT_SCRIPT", function_import_script);
+	bootstrap(ctx, "out/main.js", "out/goog/base.js");
 
 	register_global_function(ctx, "CONSOLE_LOG", function_console_log);
 	register_global_function(ctx, "CONSOLE_ERROR", function_console_error);
@@ -104,7 +249,54 @@ int main(int argc, char **argv) {
 			"console.log = CONSOLE_LOG;"\
 			"console.error = CONSOLE_ERROR;", "<init>");
 
-	bootstrap(ctx, "out/main.js", "out/goog/base.js");
+	// require app namespaces
+	evaluate_script(ctx, "goog.require('planck.repl');", "<init>");
+
+	// without this things won't work
+	evaluate_script(ctx, "var window = global;", "<init>");
+
+	register_global_function(ctx, "PLANCK_READ_FILE", function_read_file);
+	register_global_function(ctx, "PLANCK_LOAD", function_load);
+	register_global_function(ctx, "PLANCK_LOAD_DEPS_CLJS_FILES", function_load_deps_cljs_files);
+
+	register_global_function(ctx, "PLANCK_EVAL", function_eval);
+
+	register_global_function(ctx, "PLANCK_GET_TERM_SIZE", function_get_term_size);
+	register_global_function(ctx, "PLANCK_PRINT_FN", function_print_fn);
+	register_global_function(ctx, "PLANCK_PRINT_ERR_FN", function_print_err_fn);
+
+	evaluate_script(ctx, "cljs.core.set_print_fn_BANG_.call(null,PLANCK_PRINT_FN);", "<init>");
+	evaluate_script(ctx, "cljs.core.set_print_err_fn_BANG_.call(null,PLANCK_PRINT_ERR_FN);", "<init>");
+
+	bool repl = true;
+
+	{
+		JSValueRef arguments[4];
+		bool verbose = true, static_fns = true;
+		arguments[0] = JSValueMakeBoolean(ctx, repl);
+		arguments[1] = JSValueMakeBoolean(ctx, verbose);
+		JSStringRef cache_path_str = JSStringCreateWithUTF8CString(".planck_cache");
+		arguments[2] = JSValueMakeString(ctx, cache_path_str);
+		arguments[3] = JSValueMakeBoolean(ctx, static_fns);
+		JSValueRef ex = NULL;
+		JSObjectCallAsFunction(ctx, get_function(ctx, "planck.repl", "init"), JSContextGetGlobalObject(ctx), 4, arguments, &ex);
+		debug_print_value
+	("planck.repl/init", ctx, ex);
+	}
+
+	if (repl) {
+		evaluate_source(ctx, "text", "(require '[planck.repl :refer-macros [apropos dir find-doc doc source pst]])", true, "cljs.user");
+	}
+
+	evaluate_script(ctx, "goog.provide('cljs.user');", "<init>");
+	evaluate_script(ctx, "goog.require('cljs.core');", "<init>");
+
+	evaluate_script(ctx, "cljs.core._STAR_assert_STAR_ = true;", "<init>");
+
+	//evaluate_source(ctx, "text", "(empty? \"\")", true, "cljs.user");
+
+	printf("---\nrequire macros\n---\n");
+	evaluate_source(ctx, "text", "(require-macros 'planck.repl 'planck.core 'planck.shell 'planck.from.io.aviso.ansi 'clojure.template 'cljs.spec 'cljs.spec.impl.gen 'cljs.test)", true, "cljs.user");
 
 	char *script;
 	if (argc == 0) {
@@ -151,18 +343,80 @@ JSValueRef evaluate_script(JSContextRef ctx, char *script, char *source) {
 		JSStringRelease(source_ref);
 	}
 
-#ifdef DEBUG
-	if (ex != NULL) {
-		JSStringRef ex_str = to_string(ctx, ex);
-		char ex_buf[1000];
-		ex_buf[0] = '\0';
-		JSStringGetUTF8CString(ex_str, ex_buf, 1000);
-		printf("eval %s: %s\n", source, ex_buf);
-		JSStringRelease(ex_str);
-	}
-#endif
+	debug_print_value
+("evaluate_script", ctx, ex);
 
 	return val;
+}
+
+JSValueRef get_value_on_object(JSContextRef ctx, JSObjectRef obj, char *name) {
+	JSStringRef name_str = JSStringCreateWithUTF8CString(name);
+	JSValueRef val = JSObjectGetProperty(ctx, obj, name_str, NULL);
+	JSStringRelease(name_str);
+	return val;
+}
+
+JSValueRef get_value(JSContextRef ctx, char *namespace, char *name) {
+	JSValueRef ns_val = NULL;
+
+	// printf("get_value: '%s'\n", namespace);
+	int len = strlen(namespace) + 1;
+	char *ns_tmp = malloc(len * sizeof(char));
+	char **ns_tmp_start = &ns_tmp;
+	strncpy(ns_tmp, namespace, len);
+	char *ns_part = strtok(ns_tmp, ".");
+	ns_tmp = NULL;
+	while (ns_part != NULL) {
+		char *munged_ns_part = munge(ns_part);
+		if (ns_val) {
+			ns_val = get_value_on_object(ctx, JSValueToObject(ctx, ns_val, NULL), munged_ns_part);
+		} else {
+			ns_val = get_value_on_object(ctx, JSContextGetGlobalObject(ctx), munged_ns_part);
+		}
+		free(munged_ns_part); // TODO: Use a fixed buffer for this?  (Which would restrict namespace part length...)
+
+		ns_part = strtok(NULL, ".");
+	}
+	//free(ns_tmp);
+
+	return get_value_on_object(ctx, JSValueToObject(ctx, ns_val, NULL), name);
+}
+
+JSObjectRef get_function(JSContextRef ctx, char *namespace, char *name) {
+	JSValueRef val = get_value(ctx, namespace, name);
+	assert(!JSValueIsUndefined(ctx, val));
+	return JSValueToObject(ctx, val, NULL);
+}
+
+void evaluate_source(JSContextRef ctx, char *type, char *source, bool expression, char *set_ns) {
+	JSValueRef args[7];
+	int num_args = 7;
+
+	{
+		JSValueRef source_args[2];
+		JSStringRef type_str = JSStringCreateWithUTF8CString(type);
+		source_args[0] = JSValueMakeString(ctx, type_str);
+		JSStringRef source_str = JSStringCreateWithUTF8CString(source);
+		source_args[1] = JSValueMakeString(ctx, source_str);
+		args[0] = JSObjectMakeArray(ctx, 2, source_args, NULL);
+	}
+
+	args[1] = JSValueMakeBoolean(ctx, expression);
+	args[2] = JSValueMakeBoolean(ctx, false);
+	args[3] = JSValueMakeBoolean(ctx, false);
+	JSStringRef set_ns_str = JSStringCreateWithUTF8CString(set_ns);
+	args[4] = JSValueMakeString(ctx, set_ns_str);
+	JSStringRef theme_str = JSStringCreateWithUTF8CString("dumb");
+	args[5] = JSValueMakeString(ctx, theme_str);
+	args[6] = JSValueMakeNumber(ctx, 0);
+
+	JSObjectRef execute_fn = get_function(ctx, "planck.repl", "execute");
+	JSObjectRef global_obj = JSContextGetGlobalObject(ctx);
+	JSValueRef ex = NULL;
+	JSObjectCallAsFunction(ctx, execute_fn, global_obj, num_args, args, &ex);
+
+	debug_print_value
+("planck.repl/execute", ctx, ex);
 }
 
 char *munge(char *s) {
@@ -260,15 +514,21 @@ void bootstrap(JSContextRef ctx, char *deps_file_path, char *goog_base_path) {
 }
 
 char *get_contents(char *path) {
+/*#ifdef DEBUG
+	printf("get_contents(\"%s\")\n", path);
+#endif*/
+
+	char *err_prefix;
+
 	FILE *f = fopen(path, "r");
 	if (f == NULL) {
-		perror("fopen");
+		err_prefix = "fopen";
 		goto err;
 	}
 
 	struct stat f_stat;
 	if (fstat(fileno(f), &f_stat) < 0) {
-		perror("fstat");
+		err_prefix = "fstat";
 		goto err;
 	}
 
@@ -277,18 +537,19 @@ char *get_contents(char *path) {
 	fread(buf, f_stat.st_size, 1, f);
 	buf[f_stat.st_size] = '\0';
 	if (ferror(f)) {
-		perror("fread");
+		err_prefix = "fread";
 		free(buf);
 		goto err;
 	}
 
 	if (fclose(f) < 0) {
-		perror("fclose");
+		err_prefix = "fclose";
 		goto err;
 	}
 
 	return buf;
 
 err:
+	printf("get_contents(\"%s\"): %s: %s\n", path, err_prefix, strerror(errno));
 	return NULL;
 }
