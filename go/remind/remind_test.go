78d990342b37bba7a9945f4338b1256dc9b0d23e
Lucas Stadler <lu@papill0n.org>
2016-09-28 18:50:25 +0200

    Add tests

---
+++ go/remind/remind_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParse(t *testing.T) {
+	now := time.Now()
+
+	examples := []struct {
+		Input  string
+		Result time.Time
+	}{
+		{"today", now},
+		{"tomorrow", now.AddDate(0, 0, 1)},
+		//"tomorrow at 3am"
+		{"in 3 days", now.AddDate(0, 0, 3)},
+		{"in a month", now.AddDate(0, 1, 0)},
+		{"in 3 months", now.AddDate(0, 3, 0)},
+		{"next week", now.AddDate(0, 0, 7)},
+		{"next month", now.AddDate(0, 1, 0)},
+		{"in two weeks", now.AddDate(0, 0, 2*7)},
+		{"in 3 weeks", now.AddDate(0, 0, 3*7)},
+		//"2016-09-28",
+		//"3pm",
+		//"in 4 days at 10 pm",
+	}
+
+	for _, example := range examples {
+		res, err := parseTimeRelative(example.Input, now)
+		if err != nil {
+			t.Errorf("parse '%s': unexpected error: %s", err)
+		}
+
+		if !res.Equal(example.Result) {
+			t.Errorf("parse '%s': %s != %s", example.Input, res, example.Result)
+		}
+	}
+}
