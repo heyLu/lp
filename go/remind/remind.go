6d9107718c94c1b08b614c7e6ae9096ab31c977f
Lucas Stadler <lu@papill0n.org>
2016-09-28 20:59:20 +0200

    Implement basic reminding functionality

--- go/remind/remind.go
+++ go/remind/remind.go
@@ -1,32 +1,117 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
+	"sort"
+	"time"
 )
 
+type Reminder struct {
+	Date        time.Time `json:"date"`
+	Description string    `json:"description"`
+}
+
+type byDate []Reminder
+
+func (r byDate) Len() int           { return len(r) }
+func (r byDate) Less(i, j int) bool { return r[i].Date.Before(r[j].Date) }
+func (r byDate) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+
+var flags struct {
+	showAll bool
+}
+
+func init() {
+	flag.BoolVar(&flags.showAll, "all", false, "Show all reminders")
+}
+
 func main() {
-	dates := []string{
-		"today",
-		"tomorrow",
-		"tomorrow at 3am",
-		"in 3 days",
-		"in a month",
-		"in 3 months",
-		"next week",
-		"next month",
-		"in two weeks",
-		"in 3 weeks",
-		"2016-09-28",
-		"3pm",
-		"in 4 days at 10 pm",
+	flag.Parse()
+
+	cmd := "list"
+	if flag.NArg() >= 1 {
+		cmd = flag.Arg(0)
 	}
-	for _, d := range dates {
-		fmt.Printf("'%s': ", d)
-		t, err := parseTime(d)
+
+	needWrite := false
+	f, err := os.OpenFile("remind.json", os.O_RDWR, 0644)
+	if err != nil && !os.IsNotExist(err) {
+		exit(err)
+	}
+	defer f.Close()
+
+	var reminders []Reminder
+	if err == nil {
+		dec := json.NewDecoder(f)
+		err = dec.Decode(&reminders)
 		if err != nil {
-			fmt.Printf("%s\n", err)
-		} else {
-			fmt.Printf("%s\n", t)
+			exit(err)
 		}
 	}
+
+	switch cmd {
+	case "list", "l":
+		min := time.Now()
+		max := min.AddDate(0, 0, 7)
+
+		if flag.NArg() > 1 {
+			switch flag.Arg(1) {
+			case "today":
+				min = truncateHours(time.Now())
+				max = min.Add(24 * time.Hour)
+			}
+		}
+
+		sort.Sort(byDate(reminders))
+		for _, r := range reminders {
+			if flags.showAll || (r.Date.After(min) && r.Date.Before(max)) {
+				fmt.Printf("%s - %s\n", r.Date, r.Description)
+			}
+		}
+	case "add":
+		if flag.NArg() != 3 {
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		date, err := parseTime(flag.Arg(1))
+		if err != nil {
+			exit(err)
+		}
+		description := flag.Arg(2)
+		reminders = append(reminders, Reminder{Date: date, Description: description})
+		needWrite = true
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command '%s'\n", cmd)
+		os.Exit(1)
+	}
+
+	if needWrite {
+		if f == nil {
+			f, err = os.Create("remind.json")
+			if err != nil {
+				exit(err)
+			}
+		}
+
+		_, err = f.Seek(0, 0)
+		if err != nil {
+			exit(err)
+		}
+
+		enc := json.NewEncoder(f)
+		err = enc.Encode(reminders)
+		if err != nil {
+			exit(err)
+		}
+	}
+}
+
+func exit(err error) {
+	panic(err)
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	os.Exit(1)
 }
