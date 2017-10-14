5575406c98284ca10b8931536b4a3fee9b9678bb
Lucas Stadler <lu@papill0n.org>
2017-10-14 22:24:24 +0200

    Add one to things?!

--- scm/inc/compiler.scm
+++ scm/inc/compiler.scm
@@ -7,22 +7,44 @@
 (define char-shift 8)
 (define boolean-shift 7)
 
-(define (compile-program x)
-  (define (immediate-rep x)
-    (cond
-      ((integer? x) (bitwise-arithmetic-shift x fixnum-shift))
-      ((char? x) (bitwise-xor (bitwise-arithmetic-shift (char->integer x) char-shift) #b00001111))
-      ((boolean? x) (bitwise-xor (bitwise-arithmetic-shift (cond
-                                                             ((boolean=? x #t) 1)
-                                                             ((boolean=? x #f) 0))
-                                                           boolean-shift)
-                                 #b0011111))
-      ((eq? x '()) #b00101111)))
+(define (immediate-rep x)
+  (cond
+    ((integer? x) (bitwise-arithmetic-shift x fixnum-shift))
+    ((char? x) (bitwise-xor (bitwise-arithmetic-shift (char->integer x) char-shift) #b00001111))
+    ((boolean? x) (bitwise-xor (bitwise-arithmetic-shift (cond
+                                                           ((boolean=? x #t) 1)
+                                                           ((boolean=? x #f) 0))
+                                                         boolean-shift)
+                               #b0011111))
+    ((eq? x '()) #b00101111)))
 
+(define (immediate? x)
+  (or (integer? x) (char? x) (boolean? x) (eq? x '())))
+
+(define (primcall? x)
+  (list? x))
+
+(define (primcall-op x)
+  (car x))
+
+(define (primcall-operand1 x)
+  (cadr x))
+
+(define (emit-expr x)
+  (cond
+    ((immediate? x)
+     (emit "movl $~a, %eax" (immediate-rep x)))
+    ((primcall? x)
+     (case (primcall-op x)
+       ((add1)
+        (emit-expr (primcall-operand1 x))
+        (emit "addl $~a, %eax" (immediate-rep 1)))))))
+
+(define (compile-program x)
   (display ".globl scheme_entry\n\n")
   (display "scheme_entry:\n")
 
-  (emit "movl $~a, %eax" (immediate-rep x))
+  (emit-expr x)
   (emit "ret"))
 
-(compile-program '())
+(compile-program '(add1 41))
