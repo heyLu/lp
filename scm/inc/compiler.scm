a7a04c49b04334443859d7b4856d063868faf026
Lucas Stadler <lu@papill0n.org>
2017-10-14 11:14:48 +0200

    Introduce tagged literals/immediate values
    We don't test values other than integers yet, but it seems to work for
    those.  At least the test program prints 42 as before.

--- scm/inc/compiler.scm
+++ scm/inc/compiler.scm
@@ -3,11 +3,26 @@
   (display (apply format instr args))
   (display "\n"))
 
+(define fixnum-shift 2)
+(define char-shift 8)
+(define boolean-shift 7)
+
 (define (compile-program x)
+  (define (immediate-rep x)
+    (cond
+      ((integer? x) (bitwise-arithmetic-shift x fixnum-shift))
+      ((char? x) (bitwise-xor (bitwise-arithmetic-shift (char->integer x) 8) char-shift))
+      ((boolean? x) (bitwise-xor (bitwise-arithmetic-shift (cond
+                                                             ((boolean=? x #t) 1)
+                                                             ((boolean=? x #f) 0))
+                                                           boolean-shift)
+                                 #b0011111))
+      ((eq? x '()) #b00101111)))
+
   (display ".globl scheme_entry\n\n")
   (display "scheme_entry:\n")
 
-  (emit "movl $~a, %eax" x)
+  (emit "movl $~a, %eax" (immediate-rep x))
   (emit "ret"))
 
 (compile-program 42)
--- scm/inc/driver.c
+++ scm/inc/driver.c
@@ -2,9 +2,19 @@
 
 #include <stdio.h>
 
+#define fixnum_mask  3 // 11
+#define fixnum_tag   0 // 00
+#define fixnum_shift 2
+
 int scheme_entry();
 
 int main(int argc, char **argv) {
-	printf("%d\n", scheme_entry());
+	int val = scheme_entry();
+	if ((val & fixnum_mask) == fixnum_tag) {
+		printf("%d\n", val >> fixnum_shift);
+	} else {
+		printf("Error: unhandled value: %d\n", val);
+		return 1;
+	}
 	return 0;
 }
