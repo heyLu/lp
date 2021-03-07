cbacf371c9c8ca96908dc151dc607c29dfab31c1
Luna Stadler <lu@papill0n.org>
2021-03-07 14:31:16 +0100

    Experiment with SDL and tracy (which is really neat!)

---
+++ c/tracy/main.c
@@ -0,0 +1,201 @@
+// Original SDL2/OpenGL code from https://gist.github.com/jordandee/94b187bcc51df9528a2f
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/gl.h>
+
+#include "tracy/TracyC.h"
+
+#define WINDOW_WIDTH  1000
+#define WINDOW_HEIGHT 1000
+
+static const GLfloat g_vertex_buffer_data[] = {
+	-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f, // triangle 1 : end
+	1.0f, 1.0f,-1.0f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+};
+
+// One color for each vertex. They were generated randomly.
+static const GLfloat g_color_buffer_data[] = {
+	0.583f,  0.771f,  0.014f,
+	0.609f,  0.115f,  0.436f,
+	0.327f,  0.483f,  0.844f,
+	0.822f,  0.569f,  0.201f,
+	0.435f,  0.602f,  0.223f,
+	0.310f,  0.747f,  0.185f,
+	0.597f,  0.770f,  0.761f,
+	0.559f,  0.436f,  0.730f,
+	0.359f,  0.583f,  0.152f,
+	0.483f,  0.596f,  0.789f,
+	0.559f,  0.861f,  0.639f,
+	0.195f,  0.548f,  0.859f,
+	0.014f,  0.184f,  0.576f,
+	0.771f,  0.328f,  0.970f,
+	0.406f,  0.615f,  0.116f,
+	0.676f,  0.977f,  0.133f,
+	0.971f,  0.572f,  0.833f,
+	0.140f,  0.616f,  0.489f,
+	0.997f,  0.513f,  0.064f,
+	0.945f,  0.719f,  0.592f,
+	0.543f,  0.021f,  0.978f,
+	0.279f,  0.317f,  0.505f,
+	0.167f,  0.620f,  0.077f,
+	0.347f,  0.857f,  0.137f,
+	0.055f,  0.953f,  0.042f,
+	0.714f,  0.505f,  0.345f,
+	0.783f,  0.290f,  0.734f,
+	0.722f,  0.645f,  0.174f,
+	0.302f,  0.455f,  0.848f,
+	0.225f,  0.587f,  0.040f,
+	0.517f,  0.713f,  0.338f,
+	0.053f,  0.959f,  0.120f,
+	0.393f,  0.621f,  0.362f,
+	0.673f,  0.211f,  0.457f,
+	0.820f,  0.883f,  0.371f,
+	0.982f,  0.099f,  0.879f
+};
+
+int main (int ArgCount, char **Args) {
+	TracyCSetThreadName("main");
+
+	float r = 1.0;
+	float b = 1.0;
+
+	uint32_t window_flags = SDL_WINDOW_OPENGL;
+	SDL_Window *window = SDL_CreateWindow("Hello, Tracy!", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, window_flags);
+	assert(window);
+	SDL_GL_CreateContext(window);
+
+	int32_t Running = 1;
+	int32_t FullScreen = 0;
+	while (Running) {
+		TracyCZoneN(input, "input", true);
+		SDL_Event Event;
+		while (SDL_PollEvent(&Event)) {
+			if (Event.type == SDL_KEYDOWN) {
+				switch (Event.key.keysym.sym) {
+					case SDLK_ESCAPE:
+						Running = 0;
+						break;
+					case 'f':
+						FullScreen = !FullScreen;
+						if (FullScreen) {
+							SDL_SetWindowFullscreen(window, window_flags | SDL_WINDOW_FULLSCREEN_DESKTOP);
+						} else {
+							SDL_SetWindowFullscreen(window, window_flags);
+						}
+						break;
+					default:
+						break;
+				}
+			} else if (Event.type == SDL_MOUSEMOTION) {
+				r = sin(Event.motion.x / (float)WINDOW_WIDTH);
+				b = cos(Event.motion.y / (float)WINDOW_HEIGHT);
+			} else if (Event.type == SDL_QUIT) {
+				Running = 0;
+			}
+		}
+		TracyCZoneEnd(input);
+
+		/*r += 0.05;
+		b += 0.05;
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+		r = x / (float)WINDOW_WIDTH;
+		b = y / (float)WINDOW_HEIGHT;*/
+
+		TracyCZoneN(draw, "draw", true);
+		glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+		glClearColor(r, 0.f, b, 0.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		GLuint VertexArrayID;
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+
+		// This will identify our vertex buffer
+		GLuint vertexbuffer;
+		// Generate 1 buffer, put the resulting identifier in vertexbuffer
+		glGenBuffers(1, &vertexbuffer);
+		// The following commands will talk about our 'vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		// Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+				3,                  // size
+				GL_FLOAT,           // type
+				GL_FALSE,           // normalized?
+				0,                  // stride
+				(void*)0            // array buffer offset
+				);
+
+		GLuint colorbuffer;
+		glGenBuffers(1, &colorbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+		// 2nd attribute buffer : colors
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+				1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+				3,                                // size
+				GL_FLOAT,                         // type
+				GL_FALSE,                         // normalized?
+				0,                                // stride
+				(void*)0                          // array buffer offset
+				);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+
+		TracyCZoneEnd(draw);
+
+		TracyCZoneN(swap_window, "swap_window", true);
+		SDL_GL_SwapWindow(window);
+		TracyCZoneEnd(swap_window);
+
+		TracyCFrameMark;
+	}
+	return 0;
+}
