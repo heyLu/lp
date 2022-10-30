6eeba3342cf91dc6934598028b427e09f5d3a5de
Luna Stadler <lu@papill0n.org>
2022-10-30 22:48:41 +0100

    c++/tinytrace: Make a ray hit the sky!
    Or 200*100 rays if I understand this thing correctly...

---
+++ c++/tinytrace/README.md
@@ -0,0 +1,7 @@
+# tinytrace
+
+Following [Ray Tracing in a Weekend](http://in1weekend.blogspot.com/2016/01/ray-tracing-in-one-weekend.html),
+which is a delight so far, even without any actual rays being properly traced!
+
+Use `./scripts/run` to make this compile and run automatically.  (And open the
+result in an image viewer.  It's quite the nice workflow.)
---
+++ c++/tinytrace/tinytrace.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "ray.h"
+
+vec3 color(const ray& r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  float t = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+int main() {
+  int nx = 200;
+  int ny = 100;
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+  vec3 horizontal(4.0, 0.0, 0.0);
+  vec3 vertical(0.0, 2.0, 0.0);
+  vec3 origin(0.0, 0.0, 0.0);
+  for (int j = ny - 1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
+      ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+      vec3 col = color(r);
+      int ir = int(255.99*col.r());
+      int ig = int(255.99*col.g());
+      int ib = int(255.99*col.b());
+      std::cout << ir << " " << ig << " " << ib << "\n";
+    }
+  }
+}
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+  is >> t.e[0] >> t.e[1] >> t.e[2];
+  return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+  os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+  return os;
+}
+
+inline void vec3::make_unit_vector() {
+  float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+  e[0] *= k; e[1] *= k; e[2] *= k;
+}
