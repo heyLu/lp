3d53561865b78f7b01417b9a14db96dd5ddf7479
Luna Stadler <lu@papill0n.org>
2022-10-30 22:59:26 +0100

    c++/tinytrace: Have a (non-shaded) sphere!

--- c++/tinytrace/tinytrace.cpp
+++ c++/tinytrace/tinytrace.cpp
@@ -2,7 +2,19 @@
 
 #include "ray.h"
 
+bool hit_sphere(const vec3 &center, float radius, const ray &r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius*radius;
+  float discriminant = b*b - 4*a*c;
+  return discriminant > 0;
+}
+
 vec3 color(const ray& r) {
+  if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+    return vec3(1, 0, 0);
+  }
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5*(unit_direction.y() + 1.0);
   return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
--- c++/tinytrace/vec3.h
+++ c++/tinytrace/vec3.h
@@ -41,12 +41,20 @@ inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
   return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
 inline vec3 operator*(float t, const vec3 &v) {
   return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator/(vec3 v, float t) {
   return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+  return v1.e[0]*v2.e[0] + v1.e[1]*v2.e[1] + v1.e[2]*v2.e[2];
 }
 
 inline vec3 unit_vector(vec3 v) {
