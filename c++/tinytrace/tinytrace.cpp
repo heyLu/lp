4657257c9b4d43b0343f91f056b21a5897ed1cde
Luna Stadler <lu@papill0n.org>
2022-10-31 14:17:12 +0100

    c++/tinytrace: Anti-aliasing 🐌
    And also making this much slower, thus the snail.

--- c++/tinytrace/tinytrace.cpp
+++ c++/tinytrace/tinytrace.cpp
@@ -1,6 +1,7 @@
 #include <float.h>
 #include <iostream>
 
+#include "camera.h"
 #include "hitable_list.h"
 #include "sphere.h"
 
@@ -20,23 +21,27 @@ vec3 color(const ray &r, hitable *world) {
 int main() {
   int nx = 200;
   int ny = 100;
+  int ns = 100;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-  vec3 lower_left_corner(-2.0, -1.0, -1.0);
-  vec3 horizontal(4.0, 0.0, 0.0);
-  vec3 vertical(0.0, 2.0, 0.0);
-  vec3 origin(0.0, 0.0, 0.0);
   hitable *list[2];
   list[0] = new sphere(vec3(0, 0, -1), 0.5);
   list[1] = new sphere(vec3(0, -100.5, -1), 100);
   hitable *world = new hitable_list(list, 2);
+  camera cam;
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      float u = float(i) / float(nx);
-      float v = float(j) / float(ny);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 col(0, 0, 0);
 
-      vec3 p = r.point_at_parameter(2.0);
-      vec3 col = color(r, world);
+      // anti-aliasing (sample #ns rays)
+      for (int s = 0; s < ns; s++) {
+        float u = float(i + drand48()) / float(nx);
+        float v = float(j + drand48()) / float(ny);
+        ray r = cam.get_ray(u, v);
+        vec3 p = r.point_at_parameter(2.0);
+        col += color(r, world);
+      }
+      col /= float(ns);
+
       int ir = int(255.99 * col.r());
       int ig = int(255.99 * col.g());
       int ib = int(255.99 * col.b());
--- c++/tinytrace/vec3.h
+++ c++/tinytrace/vec3.h
@@ -60,4 +60,19 @@ inline float dot(const vec3 &v1, const vec3 &v2) {
   return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
 
+inline vec3 &vec3::operator+=(const vec3 &v) {
+  e[0] += v[0];
+  e[1] += v[1];
+  e[2] += v[2];
+  return *this;
+}
+
+inline vec3 &vec3::operator/=(const float t) {
+  float k = 1.0 / t; // division is expensive?
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+  return *this;
+}
+
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
