a432a8a44398248e7def71fa0226563153352b33
Luna Stadler <lu@papill0n.org>
2022-10-31 14:33:59 +0100

    c++/tinytrace: Basic diffuse material (everything is grey)
    Also, 2x speed decrease and the renderer is now recursive?

--- c++/tinytrace/tinytrace.cpp
+++ c++/tinytrace/tinytrace.cpp
@@ -6,11 +6,19 @@
 #include "hitable_list.h"
 #include "sphere.h"
 
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray &r, hitable *world) {
   hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-    return 0.5 *
-           vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   } else {
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
@@ -44,6 +52,9 @@ int main() {
         col += color(r, world);
       }
       col /= float(ns);
+
+      // gamme correct?
+      col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
 
       int ir = int(255.99 * col.r());
       int ig = int(255.99 * col.g());
