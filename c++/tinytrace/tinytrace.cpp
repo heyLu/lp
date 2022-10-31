f4bf75c3ef8f5d5b1b183d3615a2a8a668c180f5
Luna Stadler <lu@papill0n.org>
2022-10-31 17:53:14 +0100

    c++/tinytrace: Support larger image output and prepare for concurrency
    Bit all over the place here, oops.

--- c++/tinytrace/tinytrace.cpp
+++ c++/tinytrace/tinytrace.cpp
@@ -2,6 +2,8 @@
 #include <float.h>
 #include <fstream>
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 #include "camera.h"
 #include "hitable_list.h"
@@ -27,6 +29,52 @@ vec3 color(const ray &r, hitable *world, int depth) {
   }
 }
 
+class distributor {
+public:
+  distributor(int w, int h) {
+    nx = w;
+    ny = h;
+    j = ny - 1;
+    i = 0;
+    c = 0;
+  }
+
+  bool next_pixel(int &x, int &y) {
+    // for (int j = ny - 1; j >= 0; j--) {
+    //   for (int i = 0; i < nx; i++) {
+
+    if (i > nx && j < 0) {
+      return false;
+    }
+
+    lock.lock();
+
+    if (i < nx) {
+      x = i++;
+      y = j;
+    } else {
+      x = 0;
+      i = 1;
+      y = --j;
+    }
+
+    lock.unlock();
+
+    return true;
+  };
+
+  int count() { return c; }
+
+  int c;
+  int nx;
+  int ny;
+  int i;
+  int j;
+  std::mutex lock;
+};
+
+void draw_image(std::string path, int nx, int ny, vec3 *image);
+
 int main(int argc, char **argv) {
   std::string out_name = "/dev/stdout";
   bool write_partial = false;
@@ -51,62 +99,53 @@ int main(int argc, char **argv) {
   camera cam;
 
   int c = 0;
-  vec3 *image[nx * ny + 100];
+  vec3 *image = new vec3[nx * ny + 100];
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      image[c] = new vec3(254, 254, 254);
+      image[c] = vec3(254, 254, 254);
       c++;
     }
   }
+  draw_image(out_name, nx, ny, image);
+
+  int concurrency = std::thread::hardware_concurrency();
+  std::mutex next_pixel_lock;
 
   c = 0;
   auto start = std::chrono::high_resolution_clock::now();
-  for (int j = ny - 1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      vec3 col(0, 0, 0);
 
-      // anti-aliasing (sample #ns rays)
-      for (int s = 0; s < ns; s++) {
-        float u = float(i + drand48()) / float(nx);
-        float v = float(j + drand48()) / float(ny);
-        ray r = cam.get_ray(u, v);
-        vec3 p = r.point_at_parameter(2.0);
-        col += color(r, world, 0);
-      }
-      col /= float(ns);
+  auto d = new distributor(nx, ny);
+  int i, j;
+  while (d->next_pixel(i, j)) {
+    vec3 col(0, 0, 0);
 
-      // gamme correct?
-      col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
-
-      image[c] = new vec3(col.r(), col.g(), col.b());
-
-      if ((c) % int(nx * ny / 100.0) == 0) {
-        std::cerr << "."; // progress dots ✨
-
-        if (write_partial) {
-          // write updated image
-          std::ofstream out;
-          out.open(out_name, std::ios_base::trunc);
-          out << "P3\n" << nx << " " << ny << "\n255\n";
-          int cc = 0;
-          for (int y = ny - 1; y >= 0; y--) {
-            for (int x = 0; x < nx; x++) {
-              vec3 col = *image[cc];
-              int ir = std::min(int(255.99 * col.r()), 254);
-              int ig = std::min(int(255.99 * col.g()), 254);
-              int ib = std::min(int(255.99 * col.b()), 254);
-              out << ir << " " << ig << " " << ib << "\n";
-              cc++;
-            }
-          }
-          out.flush();
-          out.close();
-        }
-      }
-
-      c++;
+    // anti-aliasing (sample #ns rays)
+    for (int s = 0; s < ns; s++) {
+      float u = float(i + drand48()) / float(nx);
+      float v = float(j + drand48()) / float(ny);
+      ray r = cam.get_ray(u, v);
+      vec3 p = r.point_at_parameter(2.0);
+      col += color(r, world, 0);
     }
+    col /= float(ns);
+
+    // gamme correct?
+    col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
+
+    image[c] = vec3(col.r(), col.g(), col.b());
+
+    if ((c) % int(nx * ny / 100.0) == 0) {
+      std::cerr << "."; // progress dots ✨
+
+      if (write_partial) {
+        // write updated image
+        draw_image(out_name, nx, ny, image);
+      }
+    }
+
+    c++;
   }
+
   std::cerr << "\n";
   auto finish = std::chrono::high_resolution_clock::now();
   std::cerr << "took "
@@ -116,19 +155,22 @@ int main(int argc, char **argv) {
             << "ms"
             << "\n";
 
+  draw_image(out_name, nx, ny, image);
+}
+
+void draw_image(std::string path, int nx, int ny, vec3 *image) {
   std::ofstream out;
-  out.open(out_name, std::ios_base::trunc);
+  out.open(path, std::ios_base::trunc);
   out << "P3\n" << nx << " " << ny << "\n255\n";
-  c = 0;
+  int cc = 0;
   for (int y = ny - 1; y >= 0; y--) {
     for (int x = 0; x < nx; x++) {
-      vec3 col = *image[c];
+      vec3 col = image[cc];
       int ir = std::min(int(255.99 * col.r()), 254);
       int ig = std::min(int(255.99 * col.g()), 254);
       int ib = std::min(int(255.99 * col.b()), 254);
       out << ir << " " << ig << " " << ib << "\n";
-
-      c++;
+      cc++;
     }
   }
   out.flush();
