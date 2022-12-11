ae757dd1863549a75b6174eae2839589ee7d0547
Luna Stadler <lu@papill0n.org>
2022-12-11 13:19:44 +0100

    svelte-scribbles: Add some settings to control the style

--- js/svelte-scribbles/src/lib/Drawing.svelte
+++ js/svelte-scribbles/src/lib/Drawing.svelte
@@ -1,5 +1,8 @@
 <script>
+  let artsyMode = false;
   let isDrawing = false;
+  let color = 'black';
+  let lineWidth = 1;
   let lastEv = null;
 
   const startDrawing = (/** @type MouseEvent */ ev) => {
@@ -27,15 +30,25 @@
 
     const cv = document.querySelector("canvas");
     const ctx = cv.getContext("2d");
-    ctx.fillRect(ev.offsetX, ev.offsetY, 3, 3);
+    // ctx.fillRect(ev.offsetX, ev.offsetY, 3, 3);
 
     if (lastEv) {
+      // console.log(lastEv.offsetX, lastEv.offsetY, ev.offsetX, ev.offsetY);
+      ctx.lineWidth = lineWidth;
+      ctx.lineJoin = "round";
+      ctx.lineCap = "round";
+      ctx.strokeStyle = color;
+      ctx.beginPath()
       ctx.moveTo(lastEv.offsetX, lastEv.offsetY);
       ctx.lineTo(ev.offsetX, ev.offsetY);
       ctx.stroke();
     }
 
-    lastEv = ev;
+    if (artsyMode) {
+      lastEv = { offsetX: 0, offsetY: 0 };
+    } else {
+      lastEv = { offsetX: ev.offsetX, offsetY: ev.offsetY };
+    }
   }
 
   const clearDrawing = () => {
@@ -44,6 +57,19 @@
     ctx.clearRect(0, 0, 1000, 1000);
 
     isDrawing = false;
+  }
+
+  const setMode = (/** @type Event */ ev) => {
+    artsyMode = ev.target.checked;
+    lastEv = null;
+  }
+
+  const setLineWidth = (ev) => {
+    lineWidth = ev.target.value;
+  }
+
+  const setColor = (ev) => {
+    color = ev.target.value;
   }
 </script>
 
@@ -60,5 +86,15 @@
   on:mouseup={stopDrawing} on:mouseleave={stopDrawing}
   on:mousemove={draw}>
 </canvas>
+
+<div>
+  <label for="artsy">artsy mode:</label>
+  <input type="checkbox" name="artsy" on:change={setMode} />
+</div>
+
+<label for="width">line width ({lineWidth}):</label>
+<input type="range" name="width" value="1" min="1" max="10" on:change={setLineWidth} />
+
+<input type="color" name="color" on:change={setColor} />
 
 <button on:click={clearDrawing}>Clear</button>
