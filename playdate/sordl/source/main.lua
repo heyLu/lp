7e26673262b70e6a0f1ea89f029904bb21a32b22
Luna Stadler <lu@papill0n.org>
2023-08-31 22:28:43 +0200

    sordl: Play around with some silly geometry

---
+++ playdate/sordl/source/main.lua
@@ -0,0 +1,113 @@
+import "CoreLibs/animator"
+import "CoreLibs/graphics"
+import "CoreLibs/object"
+
+local gfx <const> = playdate.graphics
+
+function make(x, y)
+  local len = 10
+  return {
+    pos = {x = x, y = y},
+    arc = playdate.geometry.arc.new(0, 0, len, 0, 360),
+    dir = 10,
+    speed = 2,
+    len = len,
+    shield = false,
+
+    anim = nil,
+
+    draw = function(self)
+      gfx.setDitherPattern(0.3, playdate.graphics.image.kDitherTypeDiagonalLine)
+      -- gfx.fillRect(self.pos.x-5, self.pos.y-5, 10, 10)
+      gfx.fillCircleAtPoint(self.pos.x, self.pos.y, 5)
+      gfx.setDitherPattern(0)
+
+      if not self.shield then
+        if self.anim == nil then
+          local dx, dy = self.arc:pointOnArc(self.dir):unpack()
+          gfx.drawLine(self.pos.x, self.pos.y, self.pos.x+dx, self.pos.y+dy)
+        else
+          local dx, dy = self.arc:pointOnArc(self.anim:currentValue()):unpack()
+          gfx.drawLine(self.pos.x, self.pos.y, self.pos.x+dx, self.pos.y+dy)
+
+          if self.anim:ended() then
+            self.anim = nil
+          end
+        end
+      else
+        -- TODO: draw arc "behind" character to get an offset arc for bigger size
+        local dx, dy = self.arc:pointOnArc(self.dir):unpack()
+        local angle = (self.dir / self.arc:length()) * 360
+        gfx.drawArc(self.pos.x+dx*3, self.pos.y+dy*3, 5, angle-30, angle+30)
+      end
+    end,
+
+    move = function(self, button)
+      if self.anim ~= nil then
+        return
+      end
+
+      if playdate.buttonIsPressed(playdate.kButtonB) then
+        self.speed = 1
+      else
+        self.speed = 2
+      end
+
+      if button == playdate.kButtonLeft then
+        self.pos.x = self.pos.x - self.speed
+        self.dir = self.arc:length()*0.75
+      end
+      if button == playdate.kButtonRight then
+        self.pos.x = self.pos.x + self.speed
+        self.dir = self.arc:length()*0.25
+      end
+      if button == playdate.kButtonUp then
+        self.pos.y = self.pos.y - self.speed
+        self.dir = 0
+      end
+      if button == playdate.kButtonDown then
+        self.pos.y = self.pos.y + self.speed
+        self.dir = self.arc:length()*0.5
+      end
+    end,
+
+    attack = function(self)
+      if self.anim ~= nil then
+        return
+      end
+
+      self.anim = playdate.graphics.animator.new(700, self.dir, self.dir+self.arc:length()*0.25)
+    end,
+  }
+end
+
+local player = make(200, 120)
+
+function playdate.update()
+  gfx.clear()
+  playdate.drawFPS(380, 2)
+
+  player:draw()
+
+  if playdate.buttonIsPressed(playdate.kButtonLeft) then
+    player:move(playdate.kButtonLeft)
+  end
+  if playdate.buttonIsPressed(playdate.kButtonRight) then
+    player:move(playdate.kButtonRight)
+  end
+  if playdate.buttonIsPressed(playdate.kButtonUp) then
+    player:move(playdate.kButtonUp)
+  end
+  if playdate.buttonIsPressed(playdate.kButtonDown) then
+    player:move(playdate.kButtonDown)
+  end
+
+  if playdate.buttonJustPressed(playdate.kButtonA) then
+    player:attack()
+  end
+  if playdate.buttonIsPressed(playdate.kButtonB) then
+    player.shield = true
+  else
+    player.shield = false
+  end
+end
