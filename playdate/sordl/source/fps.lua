24a361fc192b9a29958de5ac20e909e5c319de95
Luna Stadler <lu@papill0n.org>
2023-09-09 21:37:11 +0200

    sordl: Properly scale fps graph to available height and framerate target
    This was scaled to the hard-coded height of 16fps and 30fps before.

--- playdate/sordl/source/fps.lua
+++ playdate/sordl/source/fps.lua
@@ -8,7 +8,7 @@ FPS.new = function(x, y, width, height)
   local history = {}
 
   for i=0,width,1 do
-    history[i] = 30
+    history[i] = playdate.display.getRefreshRate()
   end
 
   return {
@@ -17,22 +17,24 @@ FPS.new = function(x, y, width, height)
     line = playdate.geometry.polygon.new(width),
 
     draw = function(self)
+      local targetRate = playdate.display.getRefreshRate()
+
       self.history[self.offset] = playdate.getFPS()
       if self.history[self.offset] < 0 then
         -- playdate wrongly shows negative fps at the start?
-        self.history[self.offset] = 30
+        self.history[self.offset] = targetRate
       end
 
       gfx.drawLine(x, y+height, x, y) -- vertical axis
       gfx.drawLine(x, y+height, x+width, y+height) -- horizontal axis
 
-      local p = 60
+      local p = width
       for i=self.offset,1,-1 do
-        self.line:setPointAt(p, x+p, y+height-math.max(self.history[i]/2, 0))
+        self.line:setPointAt(p, x+p, y+height-math.max(self.history[i]/targetRate*height, 0))
         p = p-1
       end
       for i=#self.history,self.offset+1,-1 do
-        self.line:setPointAt(p, x+p, y+height-math.max(self.history[i]/2, 0))
+        self.line:setPointAt(p, x+p, y+height-math.max(self.history[i]/targetRate*height, 0))
         p = p-1
       end
 
