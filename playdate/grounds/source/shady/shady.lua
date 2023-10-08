0592596c9425c6ad2b160c6a4ee3a9213fb89b37
Luna Stadler <lu@papill0n.org>
2023-10-08 14:10:02 +0200

    grounds: Implement shading based on ncase's code (trick is to offset the angles!!!)
    These fucking offsets here: https://github.com/ncase/sight-and-light/blob/gh-pages/draft5.html#L113
    
    There's still bugs, not sure if they are with ordering or other nonsense.

--- playdate/grounds/source/shady/shady.lua
+++ playdate/grounds/source/shady/shady.lua
@@ -21,16 +21,15 @@ local function lineBetween(x1, y1, x2, y2)
   return geom.lineSegment.new(x1, y1, x1+dx*1000000, y1+dy*1000000)
 end
 
-local function intersect(origin, ray, lines)
+local function intersect(ray, lines)
   local minDist = math.huge
   local hit = nil
   local hitLine = nil
   for _, line in ipairs(lines) do
     local doesIntersect, point = ray:intersectsLineSegment(line)
     if doesIntersect then
-      local isOrigin = false --(line.x1 == origin.dx and line.y1 == origin.dy) or (line.x2 == origin.dx and line.y2 == origin.dy)
       local dist = geom.lineSegment.new(ray.x1, ray.y1, point.x, point.y):length()
-      if not isOrigin and dist < minDist then
+      if dist < minDist then
         hit = point
         hitLine = line
       end
@@ -39,122 +38,85 @@ local function intersect(origin, ray, lines)
   return hit, hitLine
 end
 
+local function haveSeen(t, val)
+  local seen = t[val]
+  if not seen then
+    t[val] = true
+  end
+  return seen
+end
+
 local function update()
   gfx.clear()
   gfx.fillRect(0, 0, 400, 240)
 
-  local endpoints = {}
-  local seen = {}
+  local uniquePoints = {}
+  local seenPoint = {}
   for _, line in ipairs(walls) do
-    gfx.drawLine(line)
-
-    local a = geom.vector2D.new(line.x1, line.y1)
-    if not seen[tostring(a)] then
-      table.insert(endpoints, a)
-      seen[tostring(a)] = true
+    if not haveSeen(seenPoint, line.x1*1000000+line.y1) then
+      table.insert(uniquePoints, geom.point.new(line.x1, line.y1))
     end
-    local b = geom.vector2D.new(line.x2, line.y2)
-    if not seen[tostring(b)] then
-      table.insert(endpoints, b)
-      seen[tostring(b)] = true
+    if not haveSeen(seenPoint, line.x2*1000000+line.y2) then
+      table.insert(uniquePoints, geom.point.new(line.x2, line.y2))
     end
   end
 
-  local zeroAngle = geom.vector2D.newPolar(200, 0)
-  table.sort(endpoints, function(a, b)
-    local angleA = zeroAngle:angleBetween(geom.lineSegment.new(200, 120, a.dx, a.dy):segmentVector())
-    local angleB = zeroAngle:angleBetween(geom.lineSegment.new(200, 120, b.dx, b.dy):segmentVector())
-    return angleA < angleB
+  local uniqueAngles = {}
+  local seenAngle = {}
+  for _, point in ipairs(uniquePoints) do
+    local angle = math.atan(point.y-player.y, point.x-player.x)
+    if not haveSeen(seenAngle, angle) then
+      -- add angle of point AND slightly offset to hit points _behind_ walls (VERY important!)
+      table.insert(uniqueAngles, angle-0.00001)
+      table.insert(uniqueAngles, angle)
+      table.insert(uniqueAngles, angle+0.00001)
+    end
+  end
+
+  -- intersect with angles of all wall endpoints
+  local intersects = {}
+  for _, angle in ipairs(uniqueAngles) do
+    local dx = math.cos(angle)
+    local dy = math.sin(angle)
+
+    local ray = lineBetween(player.x, player.y, player.x+dx, player.y+dy)
+    local hit, _ = intersect(ray, walls) -- iterates over lines, makes this O^2
+    if hit ~= nil then
+      table.insert(intersects, {point=hit, angle=angle})
+    end
+  end
+
+  table.sort(intersects, function(a, b)
+    return a.angle < b.angle
   end)
-  printTable(endpoints)
 
-  gfx.pushContext()
-  gfx.setColor(gfx.kColorWhite)
-
-  print("---")
-  local n = 1
   local points = {}
-  local currentWall = nil
-  for _, endpoint in ipairs(endpoints) do
-    local newPoint, newWall = intersect(endpoint, lineBetween(player.x, player.y, endpoint.dx, endpoint.dy), walls)
-
-    if #points == 0 then
-      print("init!", newWall)
-      -- table.insert(points, {x=newWall.x1, y=newWall.y1})
-      -- table.insert(points, {x=newWall.x2, y=newWall.y2})
-      currentWall = newWall
-    end
-
-    print("endpoint intersect", endpoint, newPoint, "wall", newWall)
-    if newWall ~= currentWall then
-      print("new wall!", #points, currentWall)
-      if #points < 2 then
-        print("supplement", newPoint)
-        points[2] = newPoint
-        -- points[2] = geom.point.new(currentWall.x2, currentWall.y2)
-      end
-      print("intersect", points[1], points[2])
-      local start, _ = intersect(player, lineBetween(player.x, player.y, points[1].x, points[1].y), {[1]=currentWall})
-      local _end, _ = intersect(player, lineBetween(player.x, player.y, points[2].x, points[2].y), {[1]=currentWall})
-      if _end == nil then
-        print("retry intersect")
-        _end, _ = intersect(player, lineBetween(player.x, player.y, currentWall.x1, currentWall.y1), {[1]=currentWall})
-      end
-      if start == nil or _end == nil then
-        print("no intersection")
-        break
-      end
-      print("tri", player.x, player.y, start.x, start.y, _end.x, _end.y)
-      gfx.fillTriangle(player.x, player.y, start.x, start.y, _end.x, _end.y)
-      -- TODO: remove (newest?) point
-      table.remove(points, 1)
-      if n > 5 then
-        print("break")
-        break
-      end
-      n += 1
-
-      currentWall = newWall
-    else --if #points < 2 then
-      print("add", newPoint)
-      table.insert(points, newPoint)
-      -- if #points > 2 then
-      --   table.remove(points, 1)
-      -- end
-    end
+  for _, intersection in ipairs(intersects) do
+    table.insert(points, intersection.point)
   end
-
-  gfx.setImageDrawMode(gfx.kDrawModeNXOR)
-  gfx.setColor(gfx.kColorXOR)
-
-  for _, point in ipairs(endpoints) do
-    gfx.drawRect(point.dx-2, point.dy-2, 5, 5)
-    -- gfx.drawText(i, point.dx+5, point.dy-5)
-  end
-
-  gfx.popContext()
 
   gfx.pushContext()
   gfx.setColor(gfx.kColorXOR)
-  gfx.setLineWidth(2)
-  for _, endpoint in ipairs(endpoints) do
-    gfx.drawLine(player.x, player.y, endpoint.dx, endpoint.dy)
-  end
+
+  local light = geom.polygon.new(table.unpack(points))
+  light:close()
+  gfx.fillPolygon(light)
+
   gfx.popContext()
 
   gfx.drawCircleAtPoint(player.x, player.y, 3)
 
   if playdate.buttonIsPressed(playdate.kButtonUp) then
-    player.y = player.y - 1
+    player.y = player.y - 2
   end
   if playdate.buttonIsPressed(playdate.kButtonDown) then
-    player.y = player.y + 1
+    player.y = player.y + 2
   end
   if playdate.buttonIsPressed(playdate.kButtonLeft) then
-    player.x = player.x - 1
+    player.x = player.x - 2
   end
   if playdate.buttonIsPressed(playdate.kButtonRight) then
-    player.x = player.x + 1
+    player.x = player.x + 2
   end
 end
 
