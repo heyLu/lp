6e834627f3c8cf75afb0a9b1893f11540108fcca
Luna Stadler <lu@papill0n.org>
2024-08-11 15:20:53 +0200

    things: Start sketching v2 storage with more types

--- go/things/storage/storage.go
+++ go/things/storage/storage.go
@@ -12,6 +12,7 @@ import (
 
 type Storage interface {
 	Query(ctx context.Context, namespace string, kind string, numFields int, args ...any) (Rows, error)
+	QueryV2(ctx context.Context, namespace string, conditions ...Condition) (Rows, error)
 	Insert(ctx context.Context, namespace string, kind string, args ...any) (*Metadata, error)
 	Close() error
 }
@@ -19,6 +20,7 @@ type Storage interface {
 type Rows interface {
 	Next() bool
 	Scan(args ...any) (*Metadata, error)
+	ScanV2(row *Row) error
 	Close() error
 }
 
@@ -38,6 +40,11 @@ func NewDBStorage(ctx context.Context, dsn string) (Storage, error) {
 	}
 
 	_, err = db.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS things (namespace TEXT, kind TEXT, tags TEXT, date_created INT, date_modified INT, id INT, value1 TEXT, value2 TEXT, value3 TEXT, value4 TEXT, value5 TEXT, value6 TEXT, value7 TEXT, value8 TEXT, value9 TEXT)")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS things_v2 (namespace TEXT NOT NULL, kind TEXT NOT NULL, id INTEGER NOT NULL, summary TEXT NOT NULL, content TEXT, ref TEXT, number INTEGER, float REAL, bool INTEGER, time INTEGER, fields_json BLOB, tags TEXT NOT NULL, date_created INTEGER NOT NULL, date_modified INTEGER NOT NULL, PRIMARY KEY (namespace, kind, id))")
 	if err != nil {
 		return nil, err
 	}
@@ -202,4 +209,75 @@ func tagsFromString(s string) []string {
 
 func (dbs *dbStorage) Close() error {
 	return dbs.db.Close()
+}
+
+// v2 sketch
+
+type Condition struct {
+	expr string
+	args []any
+}
+
+func (c Condition) Expr() string { return c.expr }
+func (c Condition) Args() []any  { return c.args }
+
+func Kind(kind string) Condition { return Condition{expr: "kind = ?", args: []any{kind}} }
+
+func (dbs *dbStorage) QueryV2(ctx context.Context, namespace string, conditions ...Condition) (Rows, error) {
+	query := "SELECT namespace, kind, id, summary, content, ref, number, float, bool, time, jsonb(fields_json), tags, date_created, date_modified FROM things_v2 WHERE namespace = ?"
+	queryArgs := []any{namespace}
+
+	for _, condition := range conditions {
+		query += " " + condition.expr
+		args := condition.args
+		if len(args) > 0 {
+			queryArgs = append(queryArgs, args...)
+		}
+	}
+
+	rows, err := dbs.db.QueryContext(ctx, query, queryArgs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dbRows{rows: rows}, nil
+}
+
+type Row struct {
+	Metadata
+
+	Summary string
+	Content sql.NullString
+	Ref     sql.NullString
+	Number  sql.NullInt64
+	Float   sql.NullFloat64
+	Bool    sql.NullBool
+	Time    sql.NullTime
+
+	Fields map[string]any
+}
+
+func (dbr *dbRows) ScanV2(row *Row) error {
+	var fieldsRaw interface{}
+	var tags string
+	var dateCreated int64
+	var dateModified int64
+	err := dbr.rows.Scan(&row.Namespace, &row.Kind, &row.ID, &row.Summary, &row.Content, &row.Ref, &row.Number, &row.Float, &row.Bool, &row.Time, &fieldsRaw, &tags, &dateCreated, &dateModified)
+	if err != nil {
+		return err
+	}
+
+	row.DateCreated = time.Unix(dateCreated, 0).UTC()
+	row.DateModified = time.Unix(dateModified, 0).UTC()
+
+	row.Tags = strings.Split(tags, ",")
+	if fieldsRaw != nil {
+		fields, ok := fieldsRaw.(map[string]any)
+		if !ok {
+			return fmt.Errorf("invalid 'fields': %#v", fields)
+		}
+		row.Fields = fields
+	}
+
+	return nil
 }
---
+++ go/things/storage/storage_test.go
@@ -0,0 +1,58 @@
+package storage
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var ourEpoch = time.Date(2024, 1, 1, 1, 1, 1, 0, time.UTC)
+
+func TestQueryV2(t *testing.T) {
+	st, err := NewDBStorage(context.Background(), ":memory:")
+	require.NoError(t, err)
+
+	res, err := st.(*dbStorage).db.Exec(`INSERT INTO things_v2 (namespace, kind, id, summary, content, ref, number, float, bool, time, fields_json, tags, date_created, date_modified) VALUES
+		(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		"test", "test_thing", 1, "this is a summary", nil, nil, nil, nil, nil, nil, nil, "#test,#hello-world", ourEpoch.Unix(), ourEpoch.Unix(),
+	)
+	require.NoError(t, err)
+
+	n, err := res.RowsAffected()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), n)
+
+	rows, err := st.QueryV2(context.Background(), "test")
+	require.NoError(t, err)
+
+	numResults := 0
+	for rows.Next() {
+		numResults += 1
+
+		var row Row
+		err := rows.ScanV2(&row)
+		assert.NoError(t, err)
+		if err != nil {
+			continue
+		}
+
+		assert.Equal(t,
+			Row{
+				Metadata: Metadata{
+					Namespace:    "test",
+					Kind:         "test_thing",
+					ID:           1,
+					DateCreated:  ourEpoch,
+					DateModified: ourEpoch,
+					Tags:         []string{"#test", "#hello-world"},
+				},
+				Summary: "this is a summary",
+			},
+			row)
+	}
+
+	assert.Equal(t, 1, numResults)
+}
