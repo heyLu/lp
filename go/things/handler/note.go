f415ec24abad805eb719fd4503adbe9e5bf1969a
Luna Stadler <lu@papill0n.org>
2024-08-11 19:06:51 +0200

    things: fix note rendering and search

--- go/things/handler/note.go
+++ go/things/handler/note.go
@@ -47,7 +47,11 @@ func (nh NoteHandler) Parse(input string) (Thing, error) {
 }
 
 func (nh NoteHandler) Query(ctx context.Context, db storage.Storage, namespace string, input string) (storage.Rows, error) {
-	return db.Query(ctx, namespace, storage.Kind("note"), storage.Match("summary", input))
+	parts := strings.SplitN(input, " ", 2)
+	if len(parts) == 1 {
+		return db.Query(ctx, namespace, storage.Kind("note"))
+	}
+	return db.Query(ctx, namespace, storage.Kind("note"), storage.Match("summary", parts[1]))
 }
 
 func (nh NoteHandler) Render(ctx context.Context, row *storage.Row) (Renderer, error) {
@@ -80,6 +84,6 @@ var noteTemplate = template.Must(template.New("").Funcs(noteFuncs).Parse(`
 	<time class="meta date-created" time="{{ .DateCreated }}" title="{{ .DateCreated }}">{{ .DateCreated.Format "2006-01-02 15:04:05" }}</time>
 {{ end }}
 
-	<div>{{ markdown .Content }}</div>
+	<div>{{ markdown .Summary }}</div>
 </section>
 `))
