404ecbd6fef6e8a5f6a3094e6732388dec7204ca
Luna Stadler <lu@papill0n.org>
2025-01-09 18:00:32 +0100

    os1k: allocate memory
    and run out of it, as may be tradition.

--- os/1k/kernel.c
+++ os/1k/kernel.c
@@ -119,11 +119,33 @@ void handle_trap(struct trap_frame *f) {
         user_pc);
 }
 
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+  static paddr_t next_paddr =
+      (paddr_t)__free_ram; // static makes this a global variable??
+  paddr_t paddr = next_paddr;
+  next_paddr += n * PAGE_SIZE;
+
+  if (next_paddr > (paddr_t)__free_ram_end) {
+    PANIC("out of memory");
+  }
+
+  memset((void *)paddr, 0, n * PAGE_SIZE);
+  return paddr;
+}
+
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
   WRITE_CSR(stvec, (uint32_t)kernel_entry);
-  __asm__ __volatile__("unimp");
+
+  for (int i = 0;; i++) {
+    paddr_t new_pages = alloc_pages(1);
+    printf("alloc_pages test: addr%d=%x\n", i, new_pages);
+  }
+
+  PANIC("booted");
 }
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
