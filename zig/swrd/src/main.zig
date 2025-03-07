d659112687009721d37db680d13e854f5cc5b040
Luna Stadler <lu@papill0n.org>
2025-03-07 12:35:07 +0100

    swrd: Fix for Zig 0.14.0

--- zig/swrd/src/main.zig
+++ zig/swrd/src/main.zig
@@ -256,18 +256,18 @@ fn round_frequency(f: f32) f32 {
 }
 
 inline fn errify(value: anytype) error{SdlError}!switch (@typeInfo(@TypeOf(value))) {
-    .Bool => void,
-    .Pointer, .Optional => @TypeOf(value.?),
-    .Int => |info| switch (info.signedness) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
         .signed => @TypeOf(@max(0, value)),
         .unsigned => @TypeOf(value),
     },
     else => @compileError("unhandled type: " ++ @typeName(@TypeOf(value))),
 } {
     return switch (@typeInfo(@TypeOf(value))) {
-        .Bool => if (!value) error.SdlError,
-        .Pointer, .Optional => value orelse error.SdlError,
-        .Int => |info| switch (info.signedness) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
             .signed => if (value >= 0) @max(0, value) else error.SdlError,
             .unsigned => if (value != 0) value else error.SdlError,
         },
